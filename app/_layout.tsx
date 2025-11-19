9d4da081ef7538f9642f0bd4d2f17bdf57945bed
Foilleuse <alexis.nantermod@gmail.com>
2025-11-19 12:25:29 +0100

    Updated _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { useFrameworkReady } from '../hooks/useFrameworkReady';
 import { RootNavigator } from '../src/navigation/RootNavigator';
 import { useNotificationsSetup } from '../src/hooks/useNotificationsSetup';
@@ -7,5 +8,9 @@ export default function RootLayout() {
   useFrameworkReady();
   useNotificationsSetup();
 
-  return <RootNavigator />;
+  return (
+    <NavigationContainer>
+      <RootNavigator />
+    </NavigationContainer>
+  );
 }
--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen';
 
@@ -11,15 +10,13 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export const RootNavigator = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-          contentStyle: { backgroundColor: '#F0F8FF' },
-        }}
-      >
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+        contentStyle: { backgroundColor: '#F0F8FF' },
+      }}
+    >
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
   );
 };
