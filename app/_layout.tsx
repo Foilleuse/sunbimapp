8ec2a3975053a0abad5de003e4ca53cb6676d794
Foilleuse <alexis.nantermod@gmail.com>
2025-11-28 12:09:00 +0000

    Auto-update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,37 @@
 import { Stack } from 'expo-router';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { ErrorBoundary } from '../src/components/ErrorBoundary';
 import { AuthProvider } from '../src/contexts/AuthContext';
+// On n'importe plus ErrorBoundary pour l'instant
+// import { ErrorBoundary } from '../src/components/ErrorBoundary'; 
 
 export default function RootLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <ErrorBoundary>
+      {/* On retire les balises <ErrorBoundary> */}
         <AuthProvider>
             <Stack 
               screenOptions={{ 
                 headerShown: false,
-                animation: 'none',     // Par défaut : Pas d'animation (Pour Index -> Feed)
+                animation: 'none',
                 gestureEnabled: false 
               }}
             >
               <Stack.Screen name="index" /> 
               <Stack.Screen name="(tabs)" /> 
               
-              {/* --- LA CORRECTION EST ICI --- */}
               <Stack.Screen 
                 name="profile" 
                 options={{
-                    presentation: 'modal', // Ouvre la page comme une fenêtre pop-up
-                    animation: 'default',  // Réactive l'animation pour le profil
-                    gestureEnabled: true   // Permet de fermer en glissant vers le bas
+                    presentation: 'modal',
+                    animation: 'default',
+                    gestureEnabled: true
                 }} 
               />
               
               <Stack.Screen name="+not-found" />
             </Stack>
         </AuthProvider>
-      </ErrorBoundary>
+      {/* </ErrorBoundary> */}
     </GestureHandlerRootView>
   );
 }
