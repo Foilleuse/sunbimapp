c1977b0345933ed107583d881b8f6ea03b7d07ff
Foilleuse <alexis.nantermod@gmail.com>
2025-12-03 12:28:02 +0000

    Auto-update

--- components/haptic-tab.tsx
+++ components/haptic-tab.tsx
@@ -1,16 +1,22 @@
 import { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs';
 import { PlatformPressable } from '@react-navigation/elements';
-import * as Haptics from 'expo-haptics';
+import { Platform } from 'react-native';
+
+// Suppression de l'import manquant qui bloquait le build
+// import * as Haptics from 'expo-haptics';
 
 export function HapticTab(props: BottomTabBarButtonProps) {
   return (
     <PlatformPressable
       {...props}
       onPressIn={(ev) => {
+        // Suppression de l'appel haptique qui nécessitait le module manquant
+        /*
         if (process.env.EXPO_OS === 'ios') {
           // Add a soft haptic feedback when pressing down on the tabs.
           Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
         }
+        */
         props.onPressIn?.(ev);
       }}
     />
